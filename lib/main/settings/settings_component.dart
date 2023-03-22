14b78b1e7b84fb9093137869b59403bf648ed4e1
Mostafa Aziz <mostafa.aziz@visma.com>
2023-03-22 11:30:04 +0100

    Added stuff to settings_component.dart

--- lib/main/settings/settings_component.dart
+++ lib/main/settings/settings_component.dart
@@ -18,8 +18,80 @@ class _SettingsComponentState extends State<SettingsComponent> {
             style: context.textStyles.headline6,
           ),
         ),
-        body: ListView(
-          children: [],
+        body: CustomScrollView(
+          slivers: [
+            SliverPadding(
+              padding: const EdgeInsets.all(24.0),
+              sliver: SliverList(
+                delegate: SliverChildListDelegate(
+                  [
+                    Text(
+                      'Durations',
+                      style: context.textStyles.headlineMedium,
+                    ),
+                    const SizedBox(height: 16.0),
+                    _buildFocusDuration(context),
+                    const SizedBox(height: 16.0),
+                    _buildBreakDuration(context),
+                    const SizedBox(height: 32.0),
+                    Text(
+                      'About',
+                      style: context.textStyles.headlineMedium,
+                    ),
+                    const SizedBox(height: 16.0),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: const [
+                        Text('Version'),
+                        Spacer(),
+                        Text('1.0.0'),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
         ),
+      );
+
+  Widget _buildFocusDuration(BuildContext context) => Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            'Focus Duration',
+            style: context.textStyles.subtitle2,
+          ),
+          const SizedBox(height: 8.0),
+          SizedBox(
+            width: double.infinity,
+            child: DropdownButton(
+              items: [
+                DropdownMenuItem(child: Text('25')),
+              ],
+              onChanged: (_) {},
+            ),
+          )
+        ],
+      );
+
+  Widget _buildBreakDuration(BuildContext context) => Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            'Break Duration',
+            style: context.textStyles.subtitle2,
+          ),
+          const SizedBox(height: 8.0),
+          SizedBox(
+            width: double.infinity,
+            child: DropdownButton(
+              items: [
+                DropdownMenuItem(child: Text('25')),
+              ],
+              onChanged: (_) {},
+            ),
+          )
+        ],
       );
 }
